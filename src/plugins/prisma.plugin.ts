b72fa7d238c2df0b81880152d7dd547633a755ad
Yulia Pelypenko <yulia.pelypenko.work@gmail.com>
2025-09-21 12:20:43 +0300

    rename and refactor prisma-plagin

---
+++ src/plugins/prisma.plugin.ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from "@prisma/client";
+import type { FastifyInstance } from "fastify";
+import fp from "fastify-plugin";
+
+async function initDatabaseConnection(
+	fastify: FastifyInstance,
+): Promise<PrismaClient> {
+	const db = new PrismaClient();
+
+	try {
+		await db.$connect();
+		fastify.log.info("✅ Successfully connected to the database");
+		return db;
+	} catch (error) {
+		fastify.log.error("❌ Failed to connect to the database", error);
+		throw error;
+	}
+}
+
+const prismaPlugin = fp(
+	async (fastify) => {
+		const prisma = await initDatabaseConnection(fastify);
+
+		fastify.decorate("prisma", prisma);
+
+		fastify.addHook("onClose", async () => {
+			await fastify.prisma.$disconnect();
+		});
+	},
+	{ name: "prisma" },
+);
+
+export default prismaPlugin;
