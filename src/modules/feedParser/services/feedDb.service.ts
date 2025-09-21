6d630b98db5902d353d96aa662b010214b341efa
Yulia Pelypenko <yulia.pelypenko.work@gmail.com>
2025-09-21 15:33:29 +0300

    format FeedDbService

--- src/modules/feedParser/services/feedDb.service.ts
+++ src/modules/feedParser/services/feedDb.service.ts
@@ -1,15 +1,15 @@
 import type { Feed, Prisma, PrismaClient } from "@prisma/client";
 
 export const FeedDbService = (prisma: PrismaClient) => ({
-  async getFeedByUrl(url: string): Promise<Feed | null> {
-    return prisma.feed.findFirst({ where: { url } });
-  },
+	async getFeedByUrl(url: string): Promise<Feed | null> {
+		return prisma.feed.findFirst({ where: { url } });
+	},
 
-  async saveFeed(data: Prisma.FeedCreateInput): Promise<Feed> {
-    return prisma.feed.upsert({
-      where: { url: data.url },
-      update: data,
-      create: data,
-    });
-  },
+	async saveFeed(data: Prisma.FeedCreateInput): Promise<Feed> {
+		return prisma.feed.upsert({
+			where: { url: data.url },
+			update: data,
+			create: data,
+		});
+	},
 });
