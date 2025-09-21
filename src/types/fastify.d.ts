af5a59d0cf8e5a9461e4d52a5a0dc354ae0d534c
Yulia Pelypenko <yulia.pelypenko.work@gmail.com>
2025-09-21 12:22:01 +0300

    add type for prisma

--- src/types/fastify.d.ts
+++ src/types/fastify.d.ts
@@ -1,8 +1,10 @@
+import { PrismaClient } from "@prisma/client";
 import { Config } from "../config/schema";
 
 declare module "fastify" {
 	interface FastifyInstance {
 		config: Config;
 		pluginLoaded: (pluginName: string) => void;
+		prisma: PrismaClient;
 	}
 }
